e3788ceeee6457001575c522c896af70d0c17e18
goncalogbastos <bastos.g.goncalo@gmail.com>
2022-10-27 11:52:55 +0100

    Scraper update

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import requests as r
 import pandas as pd
 from bs4 import BeautifulSoup
 
-
-
 baseURL = "https://www.autodesk.com/autodesk-university/au-online?facet_product%5B0%5D=urn%3Aadsk.content%3Acontent%3A3324e2e0-c5c3-461d-b91a-f31517c7cb19&"
 headers = {'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 10066.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'}
 page = r.get(baseURL, headers=headers)
@@ -38,12 +36,8 @@ for page in pages:
         print("Class " + str(i) + ": " + classTitle)
 
         class_titles.append(classTitle)
-        class_links.append(link_)
-    
+        class_links.append(link_)    
 
 # Export to excel
 df = pd.DataFrame({"Class Title": class_titles, "Class Links": class_links})
 df.to_excel('AUCivil3DClasses.xlsx', sheet_name="sheet1", index=False)  
-
-
-
